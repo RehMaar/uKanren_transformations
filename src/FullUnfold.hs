f2b8a5696bbb5bee70758985cf38f1b22a1ae975
Maria Kuklina <mkdirwtf@gmail.com>
2019-07-24 18:37:03 +0300

    Add new implementation of FullUnfold strategy, but it differs from the old one

--- src/FullUnfold.hs
+++ src/FullUnfold.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 {- Test results.
 
 * doubleAppendo -- depth: 7, leafs: 4
@@ -24,6 +26,7 @@ import qualified GlobalControl as GC
 import Data.Maybe (mapMaybe)
 import Data.List (group, sort)
 import qualified Data.Set as Set
+import Data.Tuple (swap)
 
 import Text.Printf
 import DotPrinter
@@ -35,12 +38,10 @@ topLevel :: G X -> (DTree, G S, [S])
 topLevel g = let
   (lgoal, lgamma, lnames) = goalXtoGoalS g
   descendGoal     = dGoal lgoal Set.empty
-  tree = derivationStep descendGoal lgamma E.s0 Set.empty 0
+  tree = fst $ derivationStep descendGoal lgamma E.s0 Set.empty 0
   in (tree, lgoal, lnames)
 
-
-showUnified :: Disj (E.Sigma, Conj (G S)) -> String
-showUnified = concatMap (\(subst, conj) -> "(" ++ show (null subst) ++ ", " ++ show conj ++ ")")
+totalDepth = 6
 
 derivationStep
   :: DDescendGoal      -- Conjunction of invokes and substs.
@@ -48,10 +49,73 @@ derivationStep
   -> E.Sigma           -- Substitution
   -> Set.Set DGoal     -- Already seen
   -> Int -- Debug depth
-  -> DTree
+  -> (DTree, Set.Set DGoal)
 derivationStep d@(CPD.Descend goal ancs) env subst seen depth
- --  | depth >= 3
- --  = Prune d
+ -- | depth >= 4
+ -- = (Prune d, seen)
+  | CPD.variantCheck goal seen
+  = (Leaf d subst env, seen) -- We didn't add `Leaf' to the `seen' set.
+  | otherwise
+  = let
+    newAncs = Set.insert goal ancs
+    newSeen = Set.insert goal seen
+  in case fullUnfoldStep goal env subst of
+     ([], _)          -> (Fail, newSeen)
+     (uGoals, newEnv) -> let
+         (seen', ts) = foldr (\g (seen, ts) -> (:ts) <$> evalSubTree depth newEnv newAncs seen g) (newSeen, []) uGoals
+       in (Or ts subst d, seen')
+
+fullUnfoldStep :: DGoal -> E.Gamma -> E.Sigma -> ([(E.Sigma, DGoal)], E.Gamma)
+fullUnfoldStep goal env subst = let
+    (newEnv, unfoldedGoal) = unfoldAll env goal
+    n = (goalToDNF <$> unfoldedGoal)
+    -- Goal :: [DNF of each body] :: [Body DNF [[Conj]]]
+    normalizedGoal = conjOfDNFtoDNF n
+    -- Goal :: [Unified DNF] :: [Body DNF [[Conj] and Substs]]
+    unifiedGoal = (\(conj, subst) -> (subst, E.substituteConjs subst conj)) <$> unifyAll subst normalizedGoal
+  in (unifiedGoal, newEnv)
+
+evalSubTree depth _ _ seen (subst, []) = (seen, Success subst)
+evalSubTree depth env ancs seen (subst, goal)
+  | not (CPD.variantCheck goal seen)
+  , isGen goal ancs
+  = let
+      descend  = CPD.Descend goal ancs
+      newAncs  = Set.insert goal ancs
+      absGoals = GC.abstractChild ancs (subst, goal, Just env)
+      (seen', ts) = foldr (\g (seen, ts) -> (:ts) <$> evalGenSubTree depth newAncs seen g) (seen, []) absGoals
+    in (seen', And ts subst descend)
+  | otherwise
+  = let
+      descend = CPD.Descend goal ancs
+      (tree, seen') = derivationStep descend env subst seen (1 + depth)
+    in (seen', Node tree descend subst)
+
+evalGenSubTree depth ancs seen (subst, goal, gen, env) = let
+    descend = CPD.Descend goal ancs
+    (newDepth, subtree) = if null gen then (1 + depth, tree) else (2 + depth, Gen tree gen)
+    (tree, seen') = derivationStep descend env subst seen newDepth
+    -- subtree = if null gen then tree else Gen tree gen
+  in (seen', subtree)
+
+
+topLevel' :: G X -> (DTree, G S, [S])
+topLevel' g = let
+  (lgoal, lgamma, lnames) = goalXtoGoalS g
+  descendGoal     = dGoal lgoal Set.empty
+  tree = derivationStep' descendGoal lgamma E.s0 Set.empty 0
+  in (tree, lgoal, lnames)
+
+derivationStep'
+  :: DDescendGoal      -- Conjunction of invokes and substs.
+  -> E.Gamma           -- Context
+  -> E.Sigma           -- Substitution
+  -> Set.Set DGoal     -- Already seen
+  -> Int -- Debug depth
+  -> DTree
+derivationStep' d@(CPD.Descend goal ancs) env subst seen depth
+  -- | depth >= totalDepth
+  -- = Prune d
   | CPD.variantCheck goal seen
   = Leaf d subst env
   -- Unfold all.
@@ -68,7 +132,6 @@ derivationStep d@(CPD.Descend goal ancs) env subst seen depth
       newAncs = Set.insert goal ancs
       newSeen = Set.insert goal seen
     in step unifiedGoal newEnv newAncs newSeen
-
     where
       run f init [] = []
       run f init (x:xs) = let tree = f init x in tree : run f (Set.union init $ treeGoals tree) xs
@@ -87,12 +150,18 @@ derivationStep d@(CPD.Descend goal ancs) env subst seen depth
         | otherwise
         = let
             descend = CPD.Descend cs ancs'
-            tree = derivationStep descend env' subst' seen' (succ depth)
+            tree = derivationStep' descend env' subst' seen' (2 + depth)
           in Node tree descend subst'
 
-      stepGen ancs' seen' (subst'', goal'', gen'', env'') = Gen tree gen''
+      stepGen ancs' seen' (subst'', goal'', [], env'')
+        = tree 
         where
-          tree = derivationStep descend env'' subst'' seen' (succ depth)
+          tree = derivationStep' descend env'' subst'' seen' (succ depth)
+          descend = CPD.Descend goal'' ancs'
+      stepGen ancs' seen' (subst'', goal'', gen'', env'')
+        = if null gen'' then tree else Gen tree gen''
+        where
+          tree = derivationStep' descend env'' subst'' seen' (2 + depth)
           descend = CPD.Descend goal'' ancs'
 
 
@@ -101,3 +170,6 @@ unfoldAll :: E.Gamma -> Conj (G S) -> (E.Gamma, Conj (G S))
 unfoldAll gamma = foldl unfold' (gamma, [])
   where
     unfold' (gamma, goals) inv = (:goals) <$> unfold inv gamma
+
+showUnified :: Disj (E.Sigma, Conj (G S)) -> String
+showUnified = concatMap (\(subst, conj) -> "(" ++ show (null subst) ++ ", " ++ show conj ++ ")")
