5eabe154b557ba2e2a7083e9a64c5e50f1f9ffeb
Maria Kuklina <mkdirwtf@gmail.com>
2019-08-29 21:42:19 +0300

    Change corresponding to a new version

--- src/OCanrenize.hs
+++ src/OCanrenize.hs
@@ -32,7 +32,7 @@ instance OCanren v => OCanren (Term v) where
   ocanren (C "s" [x]) = printf "s (%s)" (ocanren x)
   ocanren (C "true" []) = printf "!!true"
   ocanren (C "false" []) = printf "!!false"
-  ocanren (C "z" []) = "z ()"
+  ocanren (C "z" []) = "zero"
   ocanren (C (f:o) ts) = printf "(%s)" $ (toLower f : o) ++ ' ' : printArgs (map ocanren ts)
 
 instance OCanren v => OCanren (G v) where
@@ -40,7 +40,7 @@ instance OCanren v => OCanren (G v) where
   ocanren (t1 :=:  t2)  = printf "(%s === %s)" (ocanren t1) (ocanren t2)
   ocanren (g1 :/\: g2)  = printf "(%s &&& %s)" (ocanren g1) (ocanren g2)
   ocanren (g1 :\/: g2)  = printf "(%s ||| %s)" (ocanren g1) (ocanren g2)
-  ocanren (Fresh x g )  = let (names, goal) = freshVars [x] g in printf "(fresh ((%s)) (%s))" (printArgs names) (ocanren goal)
+  ocanren (Fresh x g )  = let (names, goal) = freshVars [x] g in printf "(fresh (%s) (%s))" (printArgs names) (ocanren goal)
 --ocanren (Invoke f ts) = printf "(print_string \"%s\\n\";%s)" (f ++ concat [' ' : ocanren t | t <- ts]) (f ++ concat [' ' : ocanren t | t <- ts])
   ocanren (Invoke f ts) = printf "(%s %s)" f (printArgs $ map ocanren ts)
   ocanren (Let (n, as, b) g) = printf "let rec %s %s = %s in %s" n (printArgs as) (ocanren b) (ocanren g)
@@ -88,7 +88,7 @@ toOCanren' printer filename topLevelName environment prog =
       do
         file <- openFile filename WriteMode
         hPutStrLn file "open GT"
-        hPutStrLn file "open MiniKanren"
+        hPutStrLn file "open OCanren"
         hPutStrLn file "open Std"
         hPutStrLn file "open Nat"
         hPutStrLn file ""
